1a0876db9cad03e9420bb7bbd91206046be526ef
HenCohen888 <hencohen888@gmail.com>
2025-10-04 15:39:13 +0300

    ex02: true division, floor division, modulo with explanations

--- BasicMath.py
+++ BasicMath.py
@@ -1,5 +1,6 @@
 #the function will calculate and print the solution
 
+#Section 1
 #Q1
 print("6+2=", 6+2)
 #2nd option
@@ -14,3 +15,11 @@ print(f"6 - 2 = {6 - 2}")
 print("6*2=", 6*2)
 #2nd option
 print(f"6*2 = {6*2}")
+
+#Section 2
+#Q1
+print(f"6/2 = {6/2}")
+#Q2
+print(f"6//2 = {6//2}")
+#Q3
+print(f"6%2 = {6%2}")
