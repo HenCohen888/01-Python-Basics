021e763acc6fc41ad8c91e2c7f1f59a741825e47
HenCohen888 <hencohen888@gmail.com>
2025-10-04 14:11:23 +0300

    ex01: print result using comma and f-string

--- BasicMath.py
+++ BasicMath.py
@@ -1,4 +1,16 @@
-#the function will print the calculations
+#the function will calculate and print the solution
+
+#Q1
 print("6+2=", 6+2)
-#or
+#2nd option
 print(f"6 + 2 = {6 + 2}")
+
+#Q2
+print("6-2=", 6-2)
+#2nd option
+print(f"6 - 2 = {6 - 2}")
+
+#Q3
+print("6*2=", 6*2)
+#2nd option
+print(f"6*2 = {6*2}")
